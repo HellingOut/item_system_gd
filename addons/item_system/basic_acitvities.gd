b9223c6bba11d9d7df10d41ea91eafc4d0a87d30
HellingOut <hellingout19@gmail.com>
2025-03-17 16:14:14 +0400

    From scripts to scenes

--- addons/item_system/basic_acitvities.gd
+++ addons/item_system/basic_acitvities.gd
@@ -2,3 +2,11 @@ extends VBoxContainer
 
 func on_item_switched() -> void:
 	queue_free()
+
+func _on_print_hello_pressed() -> void:
+	print("Hello")
+
+func _on_show_item_pressed() -> void:
+	var item_image = TextureRect.new()
+	item_image.texture = InventoryManager.inventory[InventoryManager.current_item].texture
+	get_parent().add_child(item_image)
